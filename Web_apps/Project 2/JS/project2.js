e391623089085fcbb47622c4eb5d89af41eb3820
Max Murphy <maxwell.murphy@outlook.com>
2024-11-17 19:19:08 -0500

    asdf

---
+++ Web_apps/Project 2/JS/project2.js
@@ -0,0 +1,115 @@
+window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
+
+let displayTerm = "";
+function searchButtonClicked(){
+    console.log("searchButtonClicked() called");
+
+    const API_URL = "https://api.scryfall.com";
+
+
+    let url = API_URL;
+
+    let term = document.querySelector("#searchterm").value;
+    displayTerm = term;
+
+    //5
+    term = term.trim();
+
+    //6
+    term = encodeURIComponent(term);
+
+    //7
+    if (term.length < 1) return;
+
+    //8
+    url += "/cards/named?exact=" + term;
+
+
+    //10
+    document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
+
+    //11
+    console.log(url);
+
+    // 12 Request data!
+    getData(url);
+    
+}
+
+function getData(url)
+ {
+     console.log("call");
+     //1
+     let xhr = new XMLHttpRequest();
+
+     //2
+     xhr.onload = dataLoaded;
+
+     //3
+     xhr.onerror = dataError;
+
+     //4
+     xhr.open("GET", url);
+     xhr.send();
+ }
+
+ function dataLoaded(e)
+ {
+     console.log("call1");
+
+     //5
+     let xhr = e.target;
+
+     //6
+     console.log(xhr.responseText);
+
+     //7
+     let obj = JSON.parse(xhr.responseText);
+
+console.log(obj);
+
+     //8
+     if (!obj.data || obj.data.length == 0)
+     {
+         document.querySelector("#status").innerHTML = "<b>No results found for '" + displayTerm + "'</b>"
+         return;
+     }
+
+     //9
+     let results = obj.data;
+     console.log("Results.length = " + results.length)
+     let bigString = "";
+
+     //10
+     for (let i = 0; i < results.length; i++)
+     {
+         let result = results[i];
+
+         //11
+         let smallURL = result.image_uris.png;
+         if (!smallURL) smallURL = "images/no-image-found.png";
+
+         //12
+         let url = result.url;
+
+         //13
+         let line = `<div class = 'result'><a target = '_blank' href = '${url}'><img src = '${smallURL}' title = '${result.title}' alt = '${result.title}'/></a>`;
+         line += `<span><a target = '_blank' href = '${url}'>${result.title}</a><p>Rating: ${result.rating.toUpperCase()}</p></div>`;
+
+         //14
+
+         //15
+         bigString += line;
+     }
+
+     //16
+     document.querySelector("#content").innerHTML = bigString;
+
+     //17
+     document.querySelector("#status").innerHTML = "<b><p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p></b>"
+ }
+
+ function dataError(e)
+ {
+     console.log("An error ossured");
+ }
